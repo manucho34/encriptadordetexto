a244bc7d3b572d1d0106277dffb438a419ceb4f7
Manuel <manuel.orsini@hotmail.com>
2023-05-29 19:38:24 -0300

    Funcion desencriptar texto

--- index.js
+++ index.js
@@ -24,3 +24,31 @@ function encriptar() {
     }
 
 }
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if  (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito"
+        parrafo.textContent = "";
+        muñeco.src = "./imagen/Muñeco.png";
+    } else {
+        muñeco.src = "./imagen/Muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar" 
+        alert("Debes ingresar algún texto");
+ }
+
+}
+
